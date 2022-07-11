c22a3ac9e40c707af2499492a673f3fb63891552
Markus Walter <markus.walter93@gmx.de>
2022-07-11 18:23:17 +0100

    added completetime column
    the complete time column is visible in statistics tab and will be updated with current timestamp when click update

--- todoapp.py
+++ todoapp.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
@@ -10,6 +11,8 @@ class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100))
     category = db.Column(db.String(50))
+    createtime = db.Column(db.DateTime())
+    completetime = db.Column(db.DateTime())
     complete = db.Column(db.Boolean)
 
 @app.route('/')
@@ -24,7 +27,9 @@ def add():
     # add new item
     title = request.form.get("title")
     category = request.form.get("category")
-    new_todo = Todo(title=title, category=category, complete=False)
+    ts = datetime.now()
+    ts = ts.replace(second=0, microsecond=0)
+    new_todo = Todo(title=title, category=category, createtime=ts, complete=False)
     db.session.add(new_todo)
     db.session.commit()
     return redirect(url_for("index"))
@@ -33,6 +38,9 @@ def add():
 def update(todo_id):
     todo = Todo.query.filter_by(id=todo_id).first()
     todo.complete = not todo.complete
+    ts = datetime.now()
+    ts = ts.replace(second=0, microsecond=0)
+    todo.completetime = ts
     db.session.commit()
     return redirect(url_for("index"))
 
@@ -47,11 +55,17 @@ def delete(todo_id):
 def about():
     return render_template('about.html')
 
+@app.route('/statistics')
+def statistics():
+    todo_list = Todo.query.all()
+    return render_template('statistics.html', todo_list=todo_list)
+
 if __name__ =="__main__":
     db.create_all()
-
-    new_todo = Todo(title="todo 1", category="programming", complete=False)
-    db.session.add(new_todo)
-    db.session.commit()
+    # ts = datetime.now()
+    # ts = ts.replace(second=0, microsecond=0)
+    # new_todo = Todo(title="todo 1", category="programming", time=ts, complete=False)
+    # db.session.add(new_todo)
+    # db.session.commit()
 
     app.run(debug=True)
